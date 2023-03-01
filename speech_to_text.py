c35f8d5f1e0d3a255c2eb4422b2887552e7783af
hamreliusfilip <filha243@student.liu.se>
2023-03-01 16:23:49 +0100

    ProofOfConcept_SpeechText
    Fungerande speech to text på engleska med begränsad tid för inspelning.

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,5 +1,16 @@
 import whisper
+import sounddevice
+from scipy.io.wavfile import write 
+
+sr=44100
+seconds=3
+print('Recording\n')
+record_voice=sounddevice.rec(sr*seconds, samplerate=sr, channels=1) 
+sounddevice.wait()
+write('audio.mp3',sr,record_voice)
+
 
 model = whisper.load_model("base")
 result = model.transcribe("audio.mp3")
 print(result["text"])
+print('Finished')
