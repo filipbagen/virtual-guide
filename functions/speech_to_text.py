2030343d85af6901fb4e3b1c1ae2393a939ac7fb
Filip Malm-Bägén <Filip.bagen@gmail.com>
2023-03-28 11:37:51 +0200

    Create speech_to_text.py

---
+++ functions/speech_to_text.py
@@ -0,0 +1,24 @@
+import speech_recognition
+import pyttsx3
+
+def speech_to_text():
+
+    recognizer = speech_recognition.Recognizer()
+
+    while True:
+
+        try:
+
+            with speech_recognition.Microphone() as mic:
+                
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                text = recognizer.recognize_google(audio, language="sv-SE")
+                text = text.lower()
+
+                # print(f"{text}")
+                return text
+
+        except speech_recognition.UnknownValueError:
+            print("Could not understand audio")
