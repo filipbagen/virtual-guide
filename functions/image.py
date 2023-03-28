d145a9482e8865a4c3c0c4e8a6cc5eebc4a9d685
hamreliusfilip <91670593+hamreliusfilip@users.noreply.github.com>
2023-03-28 10:07:26 +0200

    BugFix
    Main körde ej pga whisper.

--- functions/image.py
+++ functions/image.py
@@ -6,11 +6,14 @@ import threading
 def draw_box(x, y, w, h, canvas, vid):
     canvas.delete("all")
     canvas.create_oval(x, y, x+w, y+h, fill='white', width=3, outline='white')
-    image = Image.open("smiley.png")
-    image = image.resize((int(w), int(h)))
-    photo = ImageTk.PhotoImage(image)
-    canvas.create_image(x + w/2, y + h/2, image=photo)
-    canvas.image = photo
+    if not hasattr(canvas, "smiley.png"): # if the image doesn't exist yet, create it
+        image = Image.open("smiley.png")
+        image = image.resize((int(w), int(h)))
+        photo = ImageTk.PhotoImage(image)
+        canvas.image_id = canvas.create_image(x + w/2, y + h/2, image=photo)
+        canvas.image = photo
+    else: # if the image already exists, update its coordinates
+        canvas.coords(canvas.image_id, x + w/2, y + h/2)
 
 def open_camera(canvas,label_widget,width, height):
     vid = cv2.VideoCapture(0)
@@ -31,3 +34,6 @@ def open_camera(canvas,label_widget,width, height):
     t = threading.Thread(target=process_video)
     t.daemon = True
     t.start()
+    
+    
+    
--- functions/talk.py
+++ functions/talk.py
@@ -3,12 +3,11 @@ from gtts import gTTS
 from playsound import playsound
 import os
 
-import whisper
-import sounddevice
-from scipy.io.wavfile import write 
+# import sounddevice
+# from scipy.io.wavfile import write 
 
-import whisper
-model = whisper.load_model("tiny")
+# import whisper
+# model = whisper.load_model("tiny")
 
 def talk(text): 
     tts = gTTS(text=text, lang='sv', slow=False)
@@ -18,14 +17,12 @@ def talk(text):
     path = os.path.abspath("tts.mp3")
     playsound(path)
     
+    
 def listen():
-    sr=44100
-    seconds=3
-    print('Recording\n')
-    record_voice=sounddevice.rec(sr*seconds, samplerate=sr, channels=1) 
-    sounddevice.wait()
-    write('audiotest.mp3',sr,record_voice)
-
-    result = model.transcribe("audio.mp3")
-    print(result["text"])
-    print('Finished')
+    print("Listening")
+    # sr=44100
+    # seconds=3
+    # print('Recording\n')
+    # record_voice=sounddevice.rec(sr*seconds, samplerate=sr, channels=1) 
+    # sounddevice.wait()
+    # write('audiotest.mp3',sr,record_voice)
