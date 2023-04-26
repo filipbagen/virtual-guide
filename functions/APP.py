60e44845fc3ebe5f62492d11665111ef09e16155
hamreliusfilip <91670593+hamreliusfilip@users.noreply.github.com>
2023-04-26 13:48:59 +0200

    Styling Update

--- functions/APP.py
+++ functions/APP.py
@@ -1,4 +1,10 @@
-from PyQt6.QtGui import QPixmap
+import cv2
+from PyQt6.QtGui import (
+    QPixmap, 
+    QTextCursor, 
+    QTextCharFormat, 
+    QColor
+) 
 from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
@@ -6,37 +12,45 @@ from PyQt6.QtWidgets import (
     QTextEdit,
     QPushButton,
     QVBoxLayout,
-    QHBoxLayout
+    QHBoxLayout,
+    QPlainTextEdit,
+    QSizePolicy
 )
 from PyQt6.QtCore import (
-    QPropertyAnimation, QSequentialAnimationGroup, QPoint, QSize, Qt,
-    QThread, pyqtSignal, pyqtSlot
+    QPropertyAnimation, 
+    QSequentialAnimationGroup, 
+    QPoint, 
+    QSize, 
+    Qt,
+    QThread, 
+    pyqtSignal, 
+    pyqtSlot
 )
-# from speech_recognition import speech_rec
-# from chat_bot import generate_text
-# from text_to_speech import talk
-# from ImageAnlysis import VideoWidget
-import cv2
 
+from speech_recognition import speech_rec
+from chat_bot import generate_text
+from text_to_speech import talk
+# from ImageAnlysis import VideoWidget
+
+counter = 0
 
 class ConversationThread(QThread):
-    input_signal = pyqtSignal(str)
-    output_signal = pyqtSignal(str)
     update_gui_signal = pyqtSignal(str)
 
-    # def run(self):
-    #     while True:
-    #         input_text = speech_rec()
-    #         self.update_gui_signal.emit(input_text)
-    #         output_text = generate_text(input_text)
-    #         self.update_gui_signal.emit(output_text)
-    #         talk(output_text)
+    def run(self):
+        while True:
+            input_text = speech_rec()
+            self.update_gui_signal.emit(input_text)
+            output_text = generate_text(input_text)
+            self.update_gui_signal.emit(output_text)
+            talk(output_text)
+    
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-
+    
         self.setWindowTitle("Virtual Guide")
         self.setStyleSheet(
             """
@@ -62,16 +76,14 @@ class MainWindow(QWidget):
         self.textEditInput.setStyleSheet(
             """
                 QTextEdit {
-                    background-color: black;
-                    color: white;
-                    font-size: 20px;
-                    font-weight: bold;
-                    font-family: Helvetica;
+                    background-color: white;
                     border-radius: 10px;
+                    font-family: Helvetica;
                 }
             """)
 
         placeholderbot = QLabel("hej")
+        placeholderbot.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         placeholderbot.setMinimumSize(500, 500)
         placeholderbot.setStyleSheet("""
                 background-color: white;
@@ -82,17 +94,26 @@ class MainWindow(QWidget):
         botContainer = QVBoxLayout(self)
         chatContainer = QVBoxLayout(self)
         buttonContainer = QHBoxLayout()
-        chatContainer.addWidget(placeholderbot)
+        chatContainer.addWidget(self.textEditInput)        
         buttonContainer.addWidget(self.buttonStart())
         buttonContainer.addWidget(self.buttonStop())
         botContainer.addWidget(label)
-        botContainer.addWidget(self.textEditInput)
+        botContainer.addWidget(placeholderbot)
         botContainer.addLayout(buttonContainer)
         appContainer.addLayout(botContainer)
         appContainer.addLayout(chatContainer)
 
         self.conversation_thread = ConversationThread()
         self.conversation_thread.update_gui_signal.connect(self.set_text)
+        
+    def set_text(self, text):
+        global counter
+        
+        if (counter % 2) == 0:
+            self.textEditInput.insertHtml("<div style='font-size: 20px; color: white; background-color: #1e90ff; padding: 20px; vertical-align:middle; '>{}</div><br />".format(text))
+        else: 
+            self.textEditInput.insertHtml("<div style='font-size: 20px; color: black; background-color: white; padding: 20px; vertical-align:middle; '>{}</div><br />".format(text))
+        counter += 1
 
     def buttonStop(self):
         button = QPushButton("KILL ME", self)
@@ -136,14 +157,9 @@ class MainWindow(QWidget):
         return button
 
     def on_button_start_clicked(self):
-        self.textEditInput.setText(self.textEditInput.toPlainText(
-        ) + "\n\nConversation started, I'm listening..\n\n")
+        self.textEditInput.insertHtml("<div style='font-size: 20px; color: white; background-color: #1e90ff; padding: 20px; vertical-align: middle;'>{}</div><br />".format("I am listening..."))
         if not self.conversation_thread.isRunning():
             self.conversation_thread.start()
-
-    def set_text(self, text):
-        self.textEditInput.append(text)
-
 
 if __name__ == '__main__':
     app = QApplication([])
